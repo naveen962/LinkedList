a8a3c841011384d7aba0cd2ecea3cb4241f44e0f
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-07 04:32:54 -0700

    program to append the elements into the linked list

---
+++ LinkedListUC3/LinkedListUC3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList<int> ll = new LinkedList<int>();
+            ll.AddFirst(56);
+            
+
+            foreach (int ele in ll)
+            {
+                Console.WriteLine("The linked list elements are : " + ele);
+                Console.WriteLine("------------");
+            }
+
+            ll.AddLast(30);
+            ll.AddLast(70);
+
+
+            foreach (int ele in ll)
+            {
+                Console.WriteLine("The linked list elements are : " + ele);
+            }
+        }
+    }
+}
